776622bca1355bc14a38ebffcde0a66b798ce7be
wladimir-georgiew <wladimir_georgiew@abv.bg>
2020-11-02 01:40:37 +0200

    FixDisplayValidationsForLoginRegister
    Fixed the validations in the register and login views. Added phone number to the register form.

--- src/Web/FastServices.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Web/FastServices.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -52,6 +52,11 @@ namespace FastServices.Web.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [RegularExpression(@"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}", ErrorMessage = "Invalid phone number")]
+            [Display(Name = "PhoneNumber")]
+            public string PhoneNumber { get; set; }
+
+            [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -75,7 +80,7 @@ namespace FastServices.Web.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, PhoneNumber = Input.PhoneNumber };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
