178fa1968d160d64661e713b8da66d6e5fba0e70
wladimir-georgiew <wladimir_georgiew@abv.bg>
2020-12-13 22:44:17 +0200

    FrontEndRefactoring

---
+++ src/Web/FastServices.Web/wwwroot/js/AdminPanelJs.js
@@ -0,0 +1,11 @@
+﻿function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("openSideBarBtn").setAttribute('onclick', 'closeNav()');
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("openSideBarBtn").setAttribute('onclick', 'openNav()');
+}
---
+++ src/Web/FastServices.Web/wwwroot/js/LayoutJs.js
@@ -0,0 +1,42 @@
+﻿//$(window).load(function() {
+    $(function () {
+        // Slideshow 4
+        $("#slider3").responsiveSlides({
+            auto: true,
+            pager: true,
+            nav: false,
+            speed: 500,
+            namespace: "callbacks",
+            before: function () {
+                $('.events').append("<li>before event fired.</li>");
+            },
+            after: function () {
+                $('.events').append("<li>after event fired.</li>");
+            }
+        })
+    });
+
+    $(document).ready(function () {
+        $('#horizontalTab').easyResponsiveTabs({
+            type: 'default', //Types: default, vertical, accordion
+            width: 'auto', //auto or any width like 600px
+            fit: true,   // 100% fit in a container
+            closed: 'accordion', // Start closed if in accordion view
+            activate: function (event) { // Callback function if tab is switched
+                var $tab = $(this);
+                var $info = $('#tabInfo');
+                var $name = $('span', $info);
+                $name.text($tab.text());
+                $info.show();
+            }
+        });
+        $('#verticalTab').easyResponsiveTabs({
+            type: 'vertical',
+            width: 'auto',
+            fit: true
+        });
+    });
+
+    toastr.options.positionClass = 'toast-top-center'
+
+    //});
