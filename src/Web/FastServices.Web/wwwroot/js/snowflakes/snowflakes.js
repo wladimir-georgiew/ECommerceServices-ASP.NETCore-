a9ee19553811d5b4b36cc497727b289f7223c58a
wladimir-georgiew <wladimir_georgiew@abv.bg>
2020-12-14 17:38:57 +0200

    SnowflakesUpdate
    Updated snowflakes and removed bullets from ul error text

---
+++ src/Web/FastServices.Web/wwwroot/js/snowflakes/snowflakes.js
@@ -0,0 +1,29 @@
+﻿//$('body').flurry({
+//    character: "❄",
+//    color: "white",
+//    frequency: 100,
+//    speed: 3000,
+//    small: 8,
+//    large: 28,
+//    wind: 40,
+//    windVariance: 20,
+//    rotation: 90,
+//    rotationVariance: 180,
+//    startOpacity: 1,
+//    endOpacity: 0,
+//    opacityEasing: "cubic-bezier(1,.3,.6,.74)",
+//    blur: true,
+//    overflow: "hidden",
+//    zIndex: 9999
+//});
+
+$('body').flurry({
+    character: '❅❆*',
+    height: 800,
+    speed: 5000,
+    wind: 200,
+    windVariance: 220,
+    large: 50,
+    small: 4,
+    frequency: 50,
+});
